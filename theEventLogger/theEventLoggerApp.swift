0c40249109285c8e72aa91ace3d8170a8ba7e6e9
lstubdal <lotte.stubdal@gmail.com>
2023-08-03 13:02:18 +0100

    Initial Commit

---
+++ theEventLogger/theEventLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  theEventLoggerApp.swift
+//  theEventLogger
+//
+//  Created by Lotte Stubdal on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct theEventLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
