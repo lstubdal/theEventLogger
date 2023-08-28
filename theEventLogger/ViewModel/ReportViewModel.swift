342a0e6ee6c58183419ed0a392bcaa80a3f87094
lstubdal <lotte.stubdal@gmail.com>
2023-08-28 12:23:56 +0200

    Update location from device

--- theEventLogger/ViewModel/ReportViewModel.swift
+++ theEventLogger/ViewModel/ReportViewModel.swift
@@ -8,26 +8,30 @@
 import Foundation
 import FirebaseDatabase
 import FirebaseDatabaseSwift
+import CoreLocation
 
 class ReportViewModel: ObservableObject {
     @Published private var reportModel = ReportModel() // new object of model
-    
+    let locationManager = CLLocationManager() // initialize manager
     private let ref = Database.database().reference() // root of database
     
-   /* var reports: [Report] {
-        return reportModel.reports // access reports
-    }
-    
-    func addReport(_ report: Report) {
-        reportModel.addReport(report)
-    } */
-    
+    // add reference
     func sendToDatabase(_ report: Report) {
+        requestUserLocation()
+        
         let encoder = JSONEncoder()
         let jsonData = try! encoder.encode(report) // to convert to json object
         let newJsonReport = String(data: jsonData, encoding: String.Encoding.utf8) // stringify it
         
         ref.child("\(report.id)").setValue(newJsonReport) // push to database
+    }
+    
+    func requestUserLocation() {
+        // get access to location
+        locationManager.startMonitoringSignificantLocationChanges() // significant location changes
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        print("location started")
     }
 }
 
